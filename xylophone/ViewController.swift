c8ace93345bbbdd60eba474da75349199d301da8
dianagreyes <dianagreyes29@gmail.com>
2023-03-02 17:15:52 -0600

    Add Sound C

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+   
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSoundC()
+        //print("presione la tecla C jeje")
+    }
+    
+    func playSoundC() {
+        guard let path = Bundle.main.path(forResource: "C", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
 
-
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    
 }
+
 
